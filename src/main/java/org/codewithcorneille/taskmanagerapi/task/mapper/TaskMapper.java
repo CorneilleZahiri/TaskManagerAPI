cd9d6d2d4967edf4702c278e8be38c98a247fd93
Corneille GBEAGA <114823099+CorneilleZahiri@users.noreply.github.com>
2026-06-17 15:51:41 -0400

    Modifier une tâche (#5)

--- src/main/java/org/codewithcorneille/taskmanagerapi/task/mapper/TaskMapper.java
+++ src/main/java/org/codewithcorneille/taskmanagerapi/task/mapper/TaskMapper.java
@@ -4,6 +4,8 @@ import org.codewithcorneille.taskmanagerapi.task.dto.TaskDto;
 import org.codewithcorneille.taskmanagerapi.task.dto.TaskDtoRegister;
 import org.codewithcorneille.taskmanagerapi.task.entity.Task;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
@@ -11,5 +13,13 @@ public interface TaskMapper {
     Task taskRegisterToTask(TaskDtoRegister taskDtoRegister);
 
     TaskDto taskToTaskDto(Task task);
+
+    Task taskDtoToTask(TaskDto taskDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateTask(TaskDtoRegister taskDtoRegister, @MappingTarget Task task);
 
 }
