c76a41a1a1c8a5e54aa59789d41ee49d731dcbdd
Corneille GBEAGA <114823099+CorneilleZahiri@users.noreply.github.com>
2026-06-17 14:17:12 -0400

    Récupérer une tâche via son ID (#3)

--- src/main/java/org/codewithcorneille/taskmanagerapi/task/exception/GlobalExceptionHandler.java
+++ src/main/java/org/codewithcorneille/taskmanagerapi/task/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package org.codewithcorneille.taskmanagerapi.task.exception;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.codewithcorneille.taskmanagerapi.share.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,10 +24,10 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(errors);
     }
 
-//    @ExceptionHandler(EntityNotFoundException.class)
-//    public ResponseEntity<ApiResponse<Void>> handleEntityNotFound(EntityNotFoundException exception) {
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.error(exception.getMessage()));
-//    }
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleEntityNotFound(EntityNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.error(exception.getMessage()));
+    }
 
 //    @ExceptionHandler(DataAlreadyExistsException.class)
 //    public ResponseEntity<ApiResponse<Void>> handleDuplicateEntry(DataAlreadyExistsException exception) {
